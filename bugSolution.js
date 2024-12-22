7acfdfae40582ced319c8cecb8e849e2ec5e9fea
BugHunter-X <askzenai@gmail.com>
2024-12-23 00:48:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,26 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+    if(isRunning){
+        intervalId = setInterval(() => {
+          setCount(prevCount => prevCount + 1);
+        }, 1000);
+    }
+    return () => {
+      clearInterval(intervalId);
+      console.log('Cleanup');
+    };
+  }, [isRunning]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setIsRunning(!isRunning)}>{isRunning? 'Stop' : 'Start'}</button>
+    </div>
+  );
+}
+```
